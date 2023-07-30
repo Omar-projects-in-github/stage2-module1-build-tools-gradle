a2e65a837509fc5f11f27df39db3eb1aeef97b30
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-30 20:54:07 +0600

    StringUtils and Utils classes have been changed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,17 +1,11 @@
 package com.epam.demo;
 
-import java.util.Iterator;
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-        Iterator<String> iterator = args.iterator();
-        while (iterator.hasNext()) {
-            String value = iterator.next();
-            if (Integer.parseInt(value) <= 0)
-                return false;
-        }
-        return true;
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,8 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        if (str == null || str.isEmpty())
+            return false;
         return Double.parseDouble(str) > 0;
     }
 }
