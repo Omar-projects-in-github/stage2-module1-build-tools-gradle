6703b270b14f4fe7a074e3f63305e851bff2ff2b
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-30 21:00:21 +0600

    StringUtils class was configured

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,10 +1,10 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        if (str == null || str.isEmpty())
-            return false;
-        return Double.parseDouble(str) > 0;
+        return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
     }
 }
