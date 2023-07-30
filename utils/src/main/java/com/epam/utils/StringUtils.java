0328d9a3fc0712a79f9e4497cae32a1e2a4e2269
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-30 21:29:05 +0600

    inside StringUtils class charAt method was added

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.math.NumberUtils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
+        return NumberUtils.isParsable(str) && str.charAt(0) != 0 &&
+                Double.parseDouble(str) > 0;
     }
 }
