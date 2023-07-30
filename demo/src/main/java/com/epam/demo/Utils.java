8e15e42d9e98c94a5aa5dfb18d78a75df75781c7
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-30 15:51:51 +0600

    Added all needed configurations in gradle files, two classes have been changed, they are StingUtils and Utils classes

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,17 @@
 package com.epam.demo;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        Iterator<String> iterator = args.iterator();
+        while (iterator.hasNext()) {
+            String value = iterator.next();
+            if (Integer.parseInt(value) <= 0)
+                return false;
+        }
+        return true;
     }
 }
